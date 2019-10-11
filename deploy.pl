8c976573152253e8b934d9681abe94a53956331e
Krispin20 <56295952+Krispin20@users.noreply.github.com>
2019-10-11 17:06:01 +0300

    Update deploy.pl

--- deploy.pl
+++ deploy.pl
@@ -4,4 +4,4 @@ echo '';
 cd /var/www/domains/v.qzo.su/ || exit > /dev/null
 git reset --hard > /dev/null
 git pull > /dev/null
-chmod -R 755 /var/www/domains/ваш_сайт > /dev/null
+chmod -R 755 /var/www/domains/v.qzo.su > /dev/null
