751a504c5ec31423ce177b9ad79ccb8fcb3357c3
Krispin20 <56295952+Krispin20@users.noreply.github.com>
2019-10-14 17:04:57 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
- * 
+ * 7
  * Author: Oleg Antipov
  */
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-ini_set('error_reporting', E_ALL);
+ini_set('error_reporting', E_ALL); 
 
 require __DIR__ . '/core/app.php';
 
