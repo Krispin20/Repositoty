c01ca2af190a0d37daa86750674ffc601728233d
Krispin20 <ulmeykin01@mail.ru>
2019-10-15 14:48:28 +0300

    NewCommit 23

--- app/notification/PublicNotificationController.php
+++ app/notification/PublicNotificationController.php
@@ -8,9 +8,10 @@
 
 class PublicNotificationController {
 
-	//авторизация пользователя
-    public function example(){ echo 1; }
 
+	//авторизация пользователя
+    public function example(){ echo $this->viewJSON($array); ; }
+$array [1,2,3,4];
     //регистрация пользователя
     public function registration(){ echo (new Authorization())->startRegistration(); }
     
