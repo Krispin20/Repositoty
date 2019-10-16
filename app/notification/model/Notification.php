23df965856bb9aed28018a9236371b8ad004d8da
Krispin20 <ulmeykin01@mail.ru>
2019-10-16 14:23:17 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -4,11 +4,11 @@ class Notification extends Model{
 
     public function example(){
 
-        $this->viewJSON( Model::table("users_person_data")->get()->sort('id', 'desc')->send() );
+         $this->viewJSON($id) Model::table("users_person_data")->get()->sort('id', 'desc')->send();
         // $stmt = self::$db->prepare("SELECT * FROM  `users_person_data` ORDER BY :field DESC");
         
-         -> get(array("id")) // список полей, которые получаем из таблицы
-         -> send(1) //отправка запроса
+         // список полей, которые получаем из таблицы
+        //отправка запроса
         // $id = "id";
         // $stmt->bindValue(":field", $id, PDO::PARAM_STR);
 
