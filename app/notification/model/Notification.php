2b6e9da308b6f5fb29facb0a63caf51e73b14b4b
Krispin20 <ulmeykin01@mail.ru>
2019-10-16 13:33:00 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -6,7 +6,14 @@ class Notification extends Model{
 
         $this->viewJSON( Model::table("users_person_data")->get()->sort('id', 'desc')->send() );
         // $stmt = self::$db->prepare("SELECT * FROM  `users_person_data` ORDER BY :field DESC");
-
+        
+$connection = new mysqli("localhost","v_login,v_De3*jd_lk","",'v_db');
+$query = "select id from dd_notification";
+$result = $connection->query($query);
+while($row = mysqli_fetch_array($result)) 
+{
+echo $row['id']."<br>\n";
+}
         // $id = "id";
         // $stmt->bindValue(":field", $id, PDO::PARAM_STR);
 
