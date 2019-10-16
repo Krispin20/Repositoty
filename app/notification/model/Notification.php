65cf2b161d06f0d2290368eec242dfc0085fb395
Krispin20 <ulmeykin01@mail.ru>
2019-10-16 14:25:26 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -5,7 +5,7 @@ class Notification extends Model{
     public function example(){
 
          Model::table("users_person_data")->get()->sort('id', 'desc')->send();
-          $this->viewJSON($data)
+          $this->viewJSON($data);
         // $stmt = self::$db->prepare("SELECT * FROM  `users_person_data` ORDER BY :field DESC");
         
          // список полей, которые получаем из таблицы
