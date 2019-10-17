127a159a4274891047a718a6de56beecda0d53a8
Krispin20 <ulmeykin01@mail.ru>
2019-10-17 12:11:41 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -6,7 +6,7 @@ class Notification extends Model{
 
          Model::table("dd_notification")->get()->sort('id', 'desc')->send();
         // $stmt = self::$db->prepare("SELECT * FROM  `users_person_data` ORDER BY :field DESC");
-           array = $arr (1,2,3,4);
+           array = $arr (1,2,3,4)
            $this->viewJSON($arr);
            echo JSON($arr);
            
