ff672550eb5e0ba0499817f17fb49baa9d5f0e6d
Krispin20 <ulmeykin01@mail.ru>
2019-10-18 14:13:15 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -4,13 +4,13 @@ class Notification extends Model{
 
     public function example(){
 
-        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => ($_GET["user_type"]))->send()
+        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => ($_GET["user_type"]))->send(){
         
         if (empty($_GET["user_type"])){
         echo "Неправильный запрос, вы не ввели значение";
         
         };
-       
+       }
     
         $this->viewJSON($p);
        
