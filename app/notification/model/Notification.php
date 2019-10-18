fef828caaaa388cf1c93ad0ddedf3c3a10c0350c
Krispin20 <ulmeykin01@mail.ru>
2019-10-18 13:09:46 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -4,7 +4,7 @@ class Notification extends Model{
 
     public function example(){
 
-        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => ($_GET["$all"])))->send();
+        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => ($_GET["user_type"])))->send();
        
     
         $this->viewJSON($p);
