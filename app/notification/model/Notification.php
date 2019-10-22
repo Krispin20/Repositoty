e1144f8ff7c9cacb0b1850a2d534bb05e7496ad3
Krispin20 <ulmeykin01@mail.ru>
2019-10-22 13:09:54 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -13,7 +13,7 @@ class Notification extends Model{
         $this->viewJSON($p);
     
     
-          $g = 666;
+          $g = 1;
           $l = Model::table("dd_notification")->edit(array("user_type" => $g ), array("id" => 1))->send();
           
           echo "$l";
