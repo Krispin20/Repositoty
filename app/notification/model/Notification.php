e94ee4c4d997a3c327cbd64b3a6a62d533dbb8bd
Krispin20 <ulmeykin01@mail.ru>
2019-10-23 13:15:34 +0300

    NewCommit 23

--- app/notification/model/Notification.php
+++ app/notification/model/Notification.php
@@ -4,9 +4,9 @@ class Notification extends Model{
 
     public function getAll(){
 
-        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => $_GET["user_type"]))->send();
+        $p = Model::table("dd_notification")->get(array("user_type","id","send_time","date"))->filter(array("user_type" => $_GET["user_type"]))->send($p);
         
-        $this->viewJSON(null);
+        $this->viewJSON($p);
          
          if(empty($_GET["user_type"])){
         echo "Неправильный запрос, вы не ввели значение";
